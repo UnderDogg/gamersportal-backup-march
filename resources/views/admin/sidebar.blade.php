0b725b96dcd3cd4f7f5862ef65225c0ef1925443
Niels Drost <nielsdrost7@gmail.com>
2016-03-27 15:31:02 +0200

    Orders and Products gone from Admin Interface

--- resources/views/admin/sidebar.blade.php
+++ resources/views/admin/sidebar.blade.php
@@ -3,14 +3,7 @@
     <li class="{{ Active::route('AdminOverview') }}"><a href="{{ route('AdminOverview') }}">Overview <span class="sr-only">(current)</span></a></li>
   </ul>
   <ul class="nav nav-sidebar">
-    <li class="{{ Active::routePattern('AdminOrder*') }}"><a href="{{ route('AdminOrderIndex') }}">Orders 
-    @if($pendingOrderCount)<span class="label label-danger">{{ $pendingOrderCount }}</span>
-    @endif
-    @if($proccessingOrderCount)<span class="label label-primary">{{ $proccessingOrderCount }}</span>
-    @endif
-    </a></li>
     <li class="{{ Active::routePattern('AdminCategory*') }}"><a href="{{ route('AdminCategoryIndex') }}">Categories</a></li>
     <li class="{{ Active::routePattern('AdminProduct*') }}"><a href="{{ route('AdminProductIndex') }}">Products</a></li>
-    <li class="{{ Active::routePattern('AdminCustomer*') }}"><a href="{{ route('AdminCustomerIndex') }}">Customers</a></li>
   </ul>
 </div>
